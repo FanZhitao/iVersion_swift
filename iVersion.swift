b496937a4ef90084d9d81d3e62f9254dfb712285
Zhitao Fan <zhitaofan@iMac-2.local>
2017-01-26 15:18:14 -0500

    Added localization bundle and iVersion class.

---
+++ iVersion.swift
@@ -0,0 +1,13 @@
+//
+//  iVersion.swift
+//  iVersion
+//
+//  Created by Zhitao Fan on 1/26/17.
+//  Copyright © 2017 Mobileware. All rights reserved.
+//
+
+import Foundation
+
+class iVersion {
+    static let sharedInstance = iVersion()
+}
